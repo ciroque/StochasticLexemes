04c87eec0c27a873c062d473845f3ad221c7de2a
Steve Wagner <ciroque@live.com>
2019-01-05 10:27:44 -0800

    Adds CORS headers

--- src/main/scala/org/ciroque/web/Service.scala
+++ src/main/scala/org/ciroque/web/Service.scala
@@ -3,12 +3,15 @@ package org.ciroque.web
 import akka.pattern.ask
 import akka.actor.{ActorRef, ActorSystem, Props}
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.model.HttpMethods._
+import akka.http.scaladsl.model.headers._
+import akka.http.scaladsl.server.{Directive0, Route}
 import akka.util.Timeout
 import com.typesafe.config.Config
 import org.ciroque.lexeme.{LexemeRequest, Lexemes, Stochastic}
 import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
 
@@ -18,7 +21,34 @@ trait Protocols {
   implicit val jsonApiFormat: RootJsonFormat[JsonApi] = jsonFormat1(JsonApi)
 }
 
-trait Service extends Protocols {
+
+trait CORSHandler{
+  private val corsResponseHeaders = List(
+    `Access-Control-Allow-Origin`.*,
+    `Access-Control-Allow-Credentials`(true),
+    `Access-Control-Allow-Headers`("Authorization",
+      "Content-Type", "X-Requested-With")
+  )
+  //this directive adds access control headers to normal responses
+  private def addAccessControlHeaders: Directive0 = {
+    respondWithHeaders(corsResponseHeaders)
+  }
+  //this handles preflight OPTIONS requests.
+  private def preflightRequestHandler: Route = options {
+    complete(HttpResponse(StatusCodes.OK).
+      withHeaders(`Access-Control-Allow-Methods`(OPTIONS, GET)))
+  }
+  // Wrap the Route with this method to enable adding of CORS headers
+  def corsHandler(r: Route): Route = addAccessControlHeaders {
+    preflightRequestHandler ~ r
+  }
+  // Helper method to add CORS headers to HttpResponse
+  // preventing duplication of CORS headers across code
+  def addCORSHeaders(response: HttpResponse):HttpResponse =
+    response.withHeaders(corsResponseHeaders)
+}
+
+trait Service extends Protocols with CORSHandler {
   import scala.concurrent.duration._
   implicit val system: ActorSystem
   implicit def executor: ExecutionContextExecutor
@@ -26,17 +56,17 @@ trait Service extends Protocols {
 //  val logger: LoggingAdapter
   implicit def requestTimeout: Timeout = Timeout(5 seconds)
   val stochastic: ActorRef = system.actorOf(Props(new Stochastic()))
-  lazy val routes: Route = {
+  lazy val routes: Route = corsHandler({
     parameters("howMany".as[Int] ? 3, "maxWordLength".as[Int] ? -1, "minWordLength".as[Int] ? -1) {
       (howMany, maxWordLength, minWordLength) =>
       pathPrefix("api") {
         path("words") {
           get {
-            val wtf = (stochastic ? LexemeRequest(howMany, maxWordLength, minWordLength)).mapTo[Lexemes]
-            complete(wtf.flatMap(l => Future(JsonApi(l))))
+            val eventualLexemes = (stochastic ? LexemeRequest(howMany, maxWordLength, minWordLength)).mapTo[Lexemes]
+            complete(eventualLexemes.flatMap(l => Future(JsonApi(l))))
           }
         }
       }
     }
-  }
+  })
 }
